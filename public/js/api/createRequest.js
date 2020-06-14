662cf1c9706e84030cc5b1963e31f48b897ccac9
Vassilyev Denis <tarapiygin@gmail.com>
2020-06-14 15:39:52 +0300

    добавлена обработка исключений

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,45 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
+    const xhr = new XMLHttpRequest;
+    let formData;
+    let param = '';
+    if (options.method === 'POST') {
+        formData = new FormData;
+        if ('data' in options) {
+            for (key in options.data) {
+                formData.set(key, options.data[key])
+            };
+        };
+    } else {
+        if ('data' in options) {
+            param += '?';
+            for (key in options.data) {
+                param += encodeURI(key) + '=';
+                param += encodeURI(options.data[key]) + '&';
+            }
+        }
+    }
+    xhr.open(options.method, options.url + param)
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === xhr.DONE && this.status === 200) {
+            if(this.response.error) options.callback(err = this.response.error);
+            else options.callback(err = null, this.response);
+        }
+    });
+    if ('headers' in options) {
+        for (key in options.headers) {
+            xhr.setRequestHeader(key, options.headers[key]);
+        }
+    };
+    xhr.responseType = options.responseType;
+    xhr.withCredentials = true;
+    try {
+        if (formData !== undefined) xhr.send(formData)
+        else xhr.send();
+        return xhr;
+    }
+    catch (e) {
+        options.callback(err = e);
+    }
 };
