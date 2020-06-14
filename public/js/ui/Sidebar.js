03638f141f616413e085d33aac05299416d15a67
Vassilyev Denis <tarapiygin@gmail.com>
2020-06-14 16:48:43 +0300

    Добавлено переключение sidebar

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,7 +18,12 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
-
+    const sidebar = document.getElementsByClassName('sidebar-mini')[0];
+    const sidebarToggle = document.getElementsByClassName('sidebar-toggle')[0];
+    sidebarToggle.addEventListener('click', ()=>{
+      sidebar.classList.toggle('sidebar-open');
+      sidebar.classList.toggle('sidebar-collapse');
+    });
   }
 
   /**
